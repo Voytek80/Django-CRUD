83ea195400b7c186f101ac28a5c79b9e572d3f81
Wojciech <wojciech.mieszczak@gmail.com>
2022-09-01 14:56:13 +0200

    Update button

--- myCRUD/employee_register/urls.py
+++ myCRUD/employee_register/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.employee_form),
-    path('list/', views.employee_list)
+    path('', views.employee_form, name="employee_insert"), # get and post request for insert operations
+    path('<int:id>/', views.employee_form, name='employee_update'), # get and post request for update operation
+    path('list/', views.employee_list, name="employee_list") # get request to retrieve and display all records
 ]
